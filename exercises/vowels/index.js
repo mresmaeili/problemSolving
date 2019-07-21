c978ef8b51d14759a092226a60151b8de4aea343
mrEsmaeili <lvlr.esmaeili@gmail.com>
2019-07-21 06:10:40 +0300

    solved

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,40 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+
+function vowels(str) {
+  let count=0;
+  const checker = ['a', 'e', 'i', 'o', 'u'];
+
+  for (let char of str.toLowerCase()) {
+    if (checker.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
 
 module.exports = vowels;
+
+
+
+
+/*
+function vowels(str) {
+const check = 'aeiou';
+return str.toLowerCase().split('').filter(char => check.includes(char)).length;
+}
+
+
+module.exports = vowels;
+*/
+
+/* function vowels(str) {
+  const match = str.match(/[aeiou]/gi);
+  return !match ? 0 : match.length;
+}
+
+
+module.exports = vowels;
+*/
